1fb74f6082af3126aa249ffd0dcff779f83526c2
Alex Gower <alexanderpgower@gmail.com>
2023-11-13 15:27:42 +0000

    Fixing sync

--- code/experiments/neighbour_initial_and_final_energies_distribution_experiment.jl
+++ code/experiments/neighbour_initial_and_final_energies_distribution_experiment.jl
@@ -9,21 +9,9 @@ include("../probes/relaxed_anneal.jl")
 include("../tools/neighbour_initial_and_final_energies_graphs_plotter.jl")
 
 
-# Parallel Case
-# using Distributed
-# addprocs(Sys.CPU_THREADS-1)
-# using SharedArrays
-# @everywhere include("../probes/relaxed_anneal.jl")
-# @everywhere include("../tools/neighbour_initial_and_final_energies_graphs_plotter.jl")
 
+@inbounds @fastmath function neighbour_initial_and_final_energies_distribution_experiment(simulation_name::String, L::Int64, swap_move_probability::Float64, N_T::Int64, sample_temperatures::Vector{Float64}; verbose_metropolis_swap::Bool=false, normalization::String="solved", relaxation_iterations::Int64=Int(0), mixing_p_swap::Float64=0.0, collecting_swap_move_neighbours::Bool=false, neighbours_per_configuration_sample_size::Int64=0, average_sample_size_per_temperature::Int64=100, inherent_disorder::Bool=false, neighbour_moves_away::Int64=1, T_1::Float64=10.0, T_0::Float64=0.09, T_swap::Float64=3.0, temperature_increasing_anneal::Bool=false, initial_cube_configuration=nothing, parallel_anneals::Int64=1)
 
-@inbounds @fastmath function neighbour_initial_and_final_energies_distribution_experiment(simulation_name::String, L::Int64, swap_move_probability::Float64, N_T::Int64, sample_temperatures::Vector{Float64}; verbose_metropolis_swap::Bool=false, normalization::String="solved", relaxation_iterations::Int64=Int(0), mixing_p_swap::Float64=0.0, collecting_swap_move_neighbours::Bool=false, neighbours_per_configuration_sample_size::Int64=0, average_sample_size_per_temperature::Int64=100, inherent_disorder::Bool=false, neighbour_moves_away::Int64=1)
-
-    sample_temperatures = sort!(sample_temperatures)
-
-    T_1 = 10.0
-    T_0 = 0.09
-    T_swap = 3.0
     temperature_vector::Vector{Float64} = [T_1*(T_0/T_1)^(m/N_T) for m in 0:N_T]
     temperature_vector = [temperature_vector; sample_temperatures]
 
@@ -35,14 +23,31 @@ include("../tools/neighbour_initial_and_final_energies_graphs_plotter.jl")
     # Create a Rubik's cube object and run annealing function on it
     cube = RubiksCube(L)
 
-    if inherent_disorder
-        facelets = reduce(vcat, [fill(i,L^2) for i in 1:6])
-        shuffle!(facelets)
-        new_faces = reshape(facelets, 6, L, L)
-        for i in 1:6
-            cube.configuration[i][:,:] .= new_faces[i,:,:]
+    if isnothing(initial_cube_configuration)
+        if inherent_disorder
+            facelets = reduce(vcat, [fill(i,L^2) for i in 1:6])
+            shuffle!(facelets)
+            new_faces = reshape(facelets, 6, L, L)
+            for i in 1:6
+                cube.configuration[i][:,:] .= new_faces[i,:,:]
+            end
         end
+    else
+        println("Using initial cube configuration")
+        cube.configuration = initial_cube_configuration
     end
+
+    # Evaluate one or many relaxed_anneals depending on if using parallel processing or not
+    if parallel_anneals > 1
+
+        samples_per_temperature_per_anneal = Int(ceil(average_sample_size_per_temperature/parallel_anneals))
+
+        neighbour_initial_and_final_energies = SharedArray{Float64,1}(length(sample_temperatures)*neighbours_per_configuration_sample_size*samples_per_temperature_per_anneal*parallel_anneals)
+
+
+        @sync @distributed for trial in 1:parallel_anneals
+
+            printstyled("Trial: $trial \n", color=:light_blue)
 
             if relaxation_iterations == 0
                 temperature_vector, _, _, _, _, _, neighbour_initial_and_final_energies_by_temperature = relaxed_anneal!(cube, temperature_vector; swap_move_probability=swap_move_probability, T_swap=T_swap, verbose_annealing=true, verbose_metropolis_swap=verbose_metropolis_swap, mixing_p_swap=mixing_p_swap, neighbour_sample_temperatures=sample_temperatures, collecting_swap_move_neighbours=collecting_swap_move_neighbours, neighbours_per_configuration_sample_size=neighbours_per_configuration_sample_size, collect_initial_and_final_energies=true, average_sample_size_per_temperature=samples_per_temperature_per_anneal, neighbour_moves_away=neighbour_moves_away)
